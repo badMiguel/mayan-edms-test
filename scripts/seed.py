a02e72e2670411fe3fbc0c8dee0325218cee0cb0
miguel <badayosbiboy@gmail.com>
2025-09-21 22:32:29 +0930

    added tags and metadata on seeder. pretty much done

--- scripts/seed.py
+++ scripts/seed.py
@@ -21,37 +21,27 @@ def get_api_token():
     return r.json()["token"]
 
 
-def create_cabinet(name):
-    r = requests.post(
-        url=f"{BASE}/api/v4/cabinets/",
-        json={"label": name, "parent": None},
-        headers=headers,
-    )
-    r.raise_for_status()
-
-
-def create_document(path, label):
-    r = requests.post(
-        url=f"{BASE}/api/v4/documents/",
-        json={"document_type_id": 1, "label": label},
-        headers=headers,
-    )
-    r.raise_for_status()
-
-    with open(file=path, mode="rb") as file_object:
-        upload_file = requests.post(
-            url=f"{BASE}/api/v4/documents/{r.json()["id"]}/files/",
-            files={"file_new": file_object},
-            data={"action_name": "replace"},
-            headers={"Authorization": headers["Authorization"]},
-        )
-
-        upload_file.raise_for_status()
-
-
 def get_documents():
     r = requests.get(
         url=f"{BASE}/api/v4/documents/",
+        headers=headers,
+    )
+    r.raise_for_status()
+    return r.json()["results"]
+
+
+def get_metadata():
+    r = requests.get(
+        url=f"{BASE}/api/v4/metadata_types/",
+        headers=headers,
+    )
+    r.raise_for_status()
+    return r.json()["results"]
+
+
+def get_tags():
+    r = requests.get(
+        url=f"{BASE}/api/v4/tags/",
         headers=headers,
     )
     r.raise_for_status()
@@ -65,6 +55,90 @@ def get_cabinets():
     )
     r.raise_for_status()
     return r.json()["results"]
+
+
+def create_cabinet(idx):
+    r = requests.post(
+        url=f"{BASE}/api/v4/cabinets/",
+        json={"label": f"Seed Cabinet {idx + 1}", "parent": None},
+        headers=headers,
+    )
+    r.raise_for_status()
+
+
+def create_metadata(idx):
+    r = requests.post(
+        url=f"{BASE}/api/v4/metadata_types/",
+        json={"label": f"Seed Metadata {idx + 1}", "name": f"seed_metadata_{idx + 1}"},
+        headers=headers,
+    )
+    r.raise_for_status()
+
+    # configure created metadata to be optional when creating document
+    enable_metadata = requests.post(
+        # document id = 1 (default)
+        url=f"{BASE}/api/v4/document_types/1/metadata_types/",
+        json={"metadata_type_id": r.json()["id"], "required": False},
+        headers=headers,
+    )
+    enable_metadata.raise_for_status()
+
+
+def create_tags(idx):
+    r = requests.post(
+        url=f"{BASE}/api/v4/tags/",
+        json={"label": f"Seed Tag {idx + 1}", "color": f"#{str(idx + 1)*6}"},
+        headers=headers,
+    )
+    r.raise_for_status()
+
+
+def create_document(path, idx):
+    # create document
+    new_document = requests.post(
+        url=f"{BASE}/api/v4/documents/",
+        json={"document_type_id": 1, "label": f"Seed Document {idx + 1}"},
+        headers=headers,
+    )
+    new_document.raise_for_status()
+
+    # add file to the document
+    with open(file=path, mode="rb") as file_object:
+        upload_file = requests.post(
+            url=f"{BASE}/api/v4/documents/{new_document.json()["id"]}/files/",
+            files={"file_new": file_object},
+            data={"action_name": "replace"},
+            headers={"Authorization": headers["Authorization"]},
+        )
+        upload_file.raise_for_status()
+
+    # add document to cabinet
+    add_to_cabinet = requests.post(
+        url=f"{BASE}/api/v4/cabinets/{get_cabinets()[idx]["id"]}/documents/add/",
+        json={"document": new_document.json()["id"]},
+        headers=headers,
+    )
+    add_to_cabinet.raise_for_status()
+
+    # add tag to document
+
+    add_tag = requests.post(
+        url=f"{BASE}/api/v4/documents/{new_document.json()["id"]}/tags/attach/",
+        json={"tag": get_tags()[idx]["id"]},
+        headers=headers,
+    )
+    add_tag.raise_for_status()
+
+    # add metadata do document
+    add_metadata = requests.post(
+        url=f"{BASE}/api/v4/documents/{new_document.json()["id"]}/metadata/",
+        json={
+            "metadata_type_id": get_metadata()[idx]["id"],
+            "value": f"Metadata Data {idx + 1}",
+        },
+        headers=headers,
+    )
+    add_metadata.raise_for_status()
 
 
 def nuke_database():
@@ -85,6 +159,22 @@ def nuke_database():
         )
         r.raise_for_status()
 
+    # delete all metadata
+    for i in get_metadata():
+        r = requests.delete(
+            url=f"{BASE}/api/v4/metadata_types/{i["id"]}",
+            headers=headers,
+        )
+        r.raise_for_status()
+
+    # delete all tags
+    for i in get_tags():
+        r = requests.delete(
+            url=f"{BASE}/api/v4/tags/{i["id"]}",
+            headers=headers,
+        )
+        r.raise_for_status()
+
 
 if __name__ == "__main__":
     headers["Authorization"] = f"Token {get_api_token()}"
@@ -94,9 +184,19 @@ if __name__ == "__main__":
 
     if len(get_cabinets()) == 0:
         for i in range(5):
-            create_cabinet(f"Seed Cabinet {i + 1}")
+            create_cabinet(i)
+
+    if len(get_metadata()) == 0:
+        for i in range(5):
+            create_metadata(i)
+
+    if len(get_tags()) == 0:
+        for i in range(5):
+            create_tags(i)
 
     if len(get_documents()) == 0:
-        file_path = os.path.join(DIR, "tests", "data", "TestFile.pdf")
+        file_path = os.path.join(
+            DIR, "tests", "functional", "robot", "data", "TestFile.pdf"
+        )
         for i in range(5):
-            create_document(file_path, f"Seed Document {i + 1}")
+            create_document(file_path, i)
